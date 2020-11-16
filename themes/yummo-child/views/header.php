dc6d381c72dc2a9a6130b2bad7dfbfc06ffbd0a1
Trend <adv@trendsrl.net>
2020-11-16 15:41:55 +0100

    Update header.php
    Aggiunto menu secondario solo per versione desktop

--- themes/yummo-child/views/header.php
+++ themes/yummo-child/views/header.php
@@ -154,6 +154,18 @@
                 </div>
             </div>
         </div>
+        
+        <div class="row">
+        	<div id="menudesktop">
+        		<ul class="navbar-nav">
+                    <li><a href="https://www.plotterusati.it/ricerca.html" target="_self"><i class="glyphicon glyphicon-search"></i>Ricerca avanzata</a></li>
+                    <li><a href="https://www.plotterusati.it/mappa.html" target="_blank"><i class="glyphicon glyphicon-globe"></i>Ricerca sulla mappa</a></li>
+                    <li><a href="https://www.plotterusati.it/all" target="_self"><i class="glyphicon glyphicon-list"></i>Tutti gli annunci</a></li>
+                    <li class=""><a href="http://www.auth-privacy.com/?domain=https://www.plotterusati.it" target="_blank"><i class="fa fa-envelope-o"></i>Iscriviti alla Newsletter</a></li>
+                </ul>
+        	</div>
+        </div>
+        
     </div>
 </section>
 <?endif?>
