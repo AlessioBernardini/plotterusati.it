f612e83b10eba677919b111839aa600cb88c1b2f
Trend <46828358+AlessioBernardini@users.noreply.github.com>
2021-07-07 12:00:19 +0200

    Inserita Chat Facebook Messenger

--- themes/yummo-child/views/main.php
+++ themes/yummo-child/views/main.php
@@ -31,6 +31,33 @@
     </style>
 </head>
     <body data-spy="scroll" data-target=".subnav" data-offset="50" class="<?=(strtolower(Request::current()->controller())!=='faq' AND Theme::get('fixed_toolbar')==1)?'':'body_fixed'?>">
+    <!-- Messenger Plug-in della chat Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Plug-in della chat code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "110096181336704");
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v11.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/it_IT/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
     <?=View::factory('alert_terms')?>
     <div id="outer-wrap">
     <div id="inner-wrap">
