41c85aec7ee03880babc9c919078990c13ec4a31
Trend <46828358+AlessioBernardini@users.noreply.github.com>
2023-01-10 16:57:22 +0100

    Update moderate.php
    Fix css menu dropdown backend

--- themes/yummo-child/views/oc-panel/pages/moderate.php
+++ themes/yummo-child/views/oc-panel/pages/moderate.php
@@ -67,7 +67,7 @@
                                                         </svg>
                                                         </button>
                                                     </div>
-                                                    <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg z-10">
+                                                    <div style="position:fixed;right: 70px;" x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg z-10">
                                                         <div class="rounded-md bg-white shadow-xs">
                                                             <div class="py-1">
                                                                 <button
@@ -185,7 +185,7 @@
                                                             </svg>
                                                             </button>
                                                         </div>
-                                                        <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg z-10">
+                                                        <div style="position:fixed;right: 70px;" x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg z-10">
                                                             <div class="rounded-md bg-white shadow-xs">
                                                                 <div class="py-1">
                                                                     <?if($ad->status != Model_Ad::STATUS_SPAM):?>
